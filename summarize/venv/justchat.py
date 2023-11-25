451194d8042340224decdf9d866dc863e73fa502
ee <ezra@tamu.edu>
2023-11-24 22:18:22 -0600

    committing to switch

--- summarize/venv/justchat.py
+++ summarize/venv/justchat.py
@@ -1,6 +1,6 @@
 from openai import OpenAI
 
-client = OpenAI(api_key='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') #not sure but whatever
+client = OpenAI(api_key='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') #not sure but whatever
 # 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 - tony
 # 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 - key 2
 # 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 - key 3
@@ -18,7 +18,7 @@ def get_summary(text):
         #response = openai.Completion.create(
             model="text-davinci-003",  # Model you're using
             #model="gpt-4",
-            prompt=f"{text}\n\n Summarize the former text that I inputted, but then explain how it relates to Texas A&M Univerity.",
+            prompt=f"{text}\n\n Summarize the the entire text that you were given not leaving out any key details. After you have summarized it, spend 2 lines explaining how it can be related to Texas A&M Univerity.",
             temperature=1.0,  # Controls randomness - cranking up 
             max_tokens=150,  # Max length of response
             top_p=1.0,  # Diversity of output
