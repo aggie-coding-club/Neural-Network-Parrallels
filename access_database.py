2eea968818a707ee72fd06216f4479cfb6951c9d
lorguan <107538910+lorguan@users.noreply.github.com>
2023-11-27 02:54:41 -0600

    Update access_database.py

--- access_database.py
+++ access_database.py
@@ -3,17 +3,17 @@ import chromadb
 from embedd import bert_vector_maker
 
 from add_data import addmy_data
+def search_database(query_text):
+    #query_text = input('Input something for the database to use similarity search to find: ')
 
-query_text = input('Input something for the database to use similarity search to find: ')
+    query_embed = bert_vector_maker(query_text)
 
-query_embed = bert_vector_maker(query_text)
+    collection_name = 'maincollection'
 
-collection_name = 'maincollection'
+    how_many_results = 3
+    try:
+        thecollection = main_client.get_collection(collection_name)
 
-how_many_results = 3
-try:
-    thecollection = main_client.get_collection(collection_name)
-
-    results = main_client.thecollection.query(query_embeddings=query_embed, n_results=how_many_results)
-except ValueError:
-    print('The collection does not exists yet?? well it should you shouldnt be seeing this error unless our database has no data in it at all :/')
+        results = main_client.thecollection.query(query_embeddings=query_embed, n_results=how_many_results)
+    except ValueError:
+        print('The collection does not exists yet?? well it should you shouldnt be seeing this error unless our database has no data in it at all :/')
